144bad1b243922ae4569c9b7df64d203766a7827
Mustafa Morca <psychoplasma@gmail.com>
2025-12-07 21:35:56 +0900

    Add documentation to NonceManager interafce

--- lib/src/main/kotlin/io/github/psychoplasma/nonceq/NonceManager.kt
+++ lib/src/main/kotlin/io/github/psychoplasma/nonceq/NonceManager.kt
@@ -4,9 +4,28 @@ import java.math.BigInteger
 
 
 public interface NonceManager {
+    /*
+     * Gets the next valid nonce for the given address.
+     * If the nonce queue is empty, it fetches the nonce from the BlockNonceProvider.
+     * Thread-safe to call from multiple coroutines.
+     */
     public fun getNextValidNonce(address: String): BigInteger
 
+    /*
+     * Marks the given nonce as used for the specified address.
+     * Optionally, a transaction ID can be provided to associate with the nonce.
+     * The nonce will be removed from the queue upon calling [NonceManager.getNextValidNonce].
+     * Thread-safe to call from multiple coroutines.
+     */
     public fun useNonce(address: String, nonce: BigInteger, txId: String? = null)
 
+    /*
+     * Discards the given nonce for the specified address, optionally providing an error message.
+     */
     public fun discardNonce(address: String, nonce: BigInteger, errorMessage: String? = null)
+
+    /*
+     * Resets the nonce queue for the specified address.
+     */
+    public fun reset(address: String)
 }
